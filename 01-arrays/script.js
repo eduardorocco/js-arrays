43ca67508eb566c8f3bebb0ef36b09635735591c
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-17 10:22:43 +0200

    ADD: correction

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,23 +11,24 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 
-const fourthTeacher = teachers.indexOf(teachers[4])
+const fourthTeacher = teachers[3]
 
 console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-teachers[5] = "Patrick" 
+teachers[4] = "Patrick" 
 
 console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 
-const lastTeacher = teachers.splice(-1)
+const lastTeacher = teachers.pop()
 
 console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+
 const firstTeacher = teachers.shift()
 
 console.log(firstTeacher)
@@ -68,6 +69,6 @@ console.log(teachersString)
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 
-const isTeachersEmpty = teachers.length
+const isTeachersEmpty = teachers.length === 0
 
 console.log(isTeachersEmpty)
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,23 +11,37 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 
-const reversedTeachers = null;
-
-for (let i = 0; i < teachers.length; i++) {
-  
-const negativeIndex = -i - 1 
-
-const reversedTeachers = teachers[teachers.length - 1 - i]
+const reversedTeachers = teachers.toReversed()
 
 console.log(reversedTeachers)
-}
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
 const longNames = []
 
+for (let i = 0; i <teachers.length; i++){
 
+  const currentName = teachers[i]
+
+  if (currentName.length >= 5) {
+
+    longNames.push(currentName)
+
+  }
+}
+
+console.log(longNames)                                                                                                     
+ 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(1,1)
+
+const EdIndex = teachers.indexOf('Ed')
+
+if (EdIndex >= 0) {
+
+  teachers.splice(EdIndex,1)
+
+}
 
 console.log(teachers)
